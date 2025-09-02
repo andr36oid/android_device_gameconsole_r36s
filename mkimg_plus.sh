759717cdd0f173ebd3e47c439260fb16f834ac96
sonic011gamer <efimgirjeu@gmail.com>
2025-09-02 21:59:10 +0300

    Remove GApps instructions & add recovery combo and .ini

--- mkimg_plus.sh
+++ mkimg_plus.sh
@@ -19,6 +19,7 @@ else
     echo "Copying over kernel files"
     cp $OUTDIR/obj/KERNEL_OBJ/arch/arm64/boot/Image BOOT/
 	cp ../common/resizing/prebuilt/Image-resizing BOOT/
+	cp ../common/resizing/prebuilt/Image-recovery BOOT/
 	cp $OUTDIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/rockchip/rk3326-$DEVICE.dtb BOOT/rk3326-r36s-android.dtb
     cp $OUTDIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/rockchip/rk3326-$DEVICE.dtb BOOT/rk3326-rg351mplus.dtb
 	echo "Creating image file $IMGNAME..."
